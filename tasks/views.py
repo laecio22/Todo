5e1dae58e5b1c5552335c896bf9094c45a496dca
Laécio da Conceição Viana <laecon22@hotmail.com>
2019-05-05 18:38:18 -0300

    paginação ainda não completa

--- tasks/views.py
+++ tasks/views.py
@@ -3,10 +3,14 @@ from django .http import HttpResponse
 from .models import Task
 from .forms import TaskForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks_list = Task.objects.all().order_by('-created_at')
+    paginator = Paginator(tasks_list,3)
+    page = request.GET.get('page')
+    tasks = paginator.get_page(page)
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 def taskView(request, id):
