ff63c09b3fd40e3228a919da0ead5f7f1d38e2da
Laécio da Conceição Viana <laecon22@hotmail.com>
2019-05-06 11:29:47 -0300

    Paginacao final

--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -1,11 +1,9 @@
-$(document).ready(
-    function () {
-        var deleteBtn = $('.delete-btn');
+  $(document).ready(function () {
+       var deleteBtn = $('.delete-btn');
 
-        $(deleteBtn).on('click' , function (e) {
+       $(deleteBtn).on('click' , function (e) {
 
             e.preventDefault();
-
             var delLink = $(this).attr('href');
             var  result = confirm('Quer deletar esta tarefa?');
 
@@ -13,7 +11,7 @@ $(document).ready(
                 window.location.href= delLink;
             }
 
-        })
 
+       });
 
-    });
+  });
